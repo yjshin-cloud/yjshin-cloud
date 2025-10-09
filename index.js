a89f3b475815280a2c3162145b518b6e661629dc
yjshin.cloud <yjshin.cloud@gmail.com>
2025-10-09 13:39:17 +0900

    blog edit commit

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 import { writeFileSync } from "node:fs";
 import Parser from "rss-parser";
 
-/**
- * README.MD에 작성될 페이지 텍스트
- * @type {string}
- */
-let text = `# Hi there 👋
-
-## 이런 환경에 익숙해요✍🏼
-
-## 언어
-
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
-
-## 📕 Latest Blog Posts
-
-`;
-
-// rss-parser 생성
 const parser = new Parser({
   headers: {
     Accept: "application/rss+xml, application/xml, text/xml; q=0.1",
@@ -28,25 +8,96 @@ const parser = new Parser({
 });
 
 (async () => {
-  // 피드 목록
-  const feed = await parser.parseURL("https://deeprun.tistory.com/rss"); // 본인의 블로그 주소
+  try {
+    const feed = await parser.parseURL("https://deeprun.tistory.com/rss");
 
-  text += `<ul>`;
+    // 📄 README 기본 내용 (사용자 고정 섹션)
+    let text = `
+![yjshin-cloud's GitHub](https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=yjshin-cloud&theme=vue)
 
-  // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
-  for (let i = 0; i < 10; i++) {
-    const { title, link } = feed.items[i];
-    console.log(`${i + 1}번째 게시물`);
-    console.log(`추가될 제목: ${title}`);
-    console.log(`추가될 링크: ${link}`);
-    text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+<a href="mailto:yjshin.cloud@gmail.com"><img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&style=plastic&link=yjshin.cloud@gmail.com"/></a> 
+[![Velog Badge](http://img.shields.io/badge/-Velog-20C997?style=flat&logo=velog&logoColor=white&style=plastic&link=https://velog.io/@yjshin)](https://velog.io/@yjshin)
+[![Tistory Badge](https://img.shields.io/badge/Tistory-FF6A00?style=flat&logo=tistory&logoColor=white&labelColor=FF6A00&color=FF6A00)](https://deeprun.tistory.com/category/Cloud%20Engineering%20Bootcamp)
+[![TIL Blog Badge](https://img.shields.io/badge/TIL%20Blog-181717?style=flat&logo=github&logoColor=white&link=https://yjshin-cloud.github.io/TIL)](https://yjshin-cloud.github.io/TIL)
+
+
+# 🛠️ Skills
+
+![Python](https://img.shields.io/badge/Python-3776AB?logo=python&logoColor=white)
+![Django](https://img.shields.io/badge/Django-092E20?logo=django&logoColor=white)
+![Go](https://img.shields.io/badge/Go-00ADD8?logo=go&logoColor=white)
+![Java](https://img.shields.io/badge/Java-007396?logo=openjdk&logoColor=white)
+![Spring Boot](https://img.shields.io/badge/Spring%20Boot-6DB33F?logo=springboot&logoColor=white)
+![MySQL](https://img.shields.io/badge/MySQL-4479A1?logo=mysql&logoColor=white)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-4169E1?logo=postgresql&logoColor=white)
+![Linux](https://img.shields.io/badge/Linux-FCC624?logo=linux&logoColor=black)
+![AWS](https://img.shields.io/badge/Amazon%20AWS-232F3E?logo=amazonaws&logoColor=white)
+![GCP](https://img.shields.io/badge/Google%20Cloud-4285F4?logo=googlecloud&logoColor=white)
+![Azure](https://img.shields.io/badge/Microsoft%20Azure-0078D4?logo=microsoftazure&logoColor=white)
+![Docker](https://img.shields.io/badge/Docker-2496ED?logo=docker&logoColor=white)
+![Kubernetes](https://img.shields.io/badge/Kubernetes-326CE5?logo=kubernetes&logoColor=white)
+![Terraform](https://img.shields.io/badge/Terraform-7B42BC?logo=terraform&logoColor=white)
+
+<details>
+<summary>🔧 Skills detail</summary>
+
+## Languege
+<a href="https://www.python.org/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg" width="36" height="36"/></a>
+<a href="https://www.djangoproject.com/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/django-colored.svg" width="36" height="36"/></a>
+<a href="https://www.oracle.com/java/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/java-colored.svg" width="36" height="36"/></a>
+<a href="https://spring.io/projects/spring-boot" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/springboot-colored.svg" width="36" height="36"/></a>
+
+## DB
+<a href="https://www.mysql.com/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg" width="36" height="36"/></a>
+<a href="https://www.postgresql.org/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg" width="36" height="36"/></a>
+
+## Cloud
+<a href="https://aws.amazon.com/" target="_blank"><img src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/aws.png" width="36" height="36"/></a>
+<a href="https://cloud.google.com/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/googlecloud-colored.svg" width="36" height="36"/></a>
+<a href="https://azure.microsoft.com/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg" width="36" height="36"/></a>
+
+## Tool
+<a href="https://www.jetbrains.com/idea/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg" width="36" height="36"/></a>
+<a href="https://code.visualstudio.com/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/visualstudiocode-colored.svg" width="36" height="36"/></a>
+<a href="https://www.vim.org/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vim-colored.svg" width="36" height="36"/></a>
+
+## OS
+<a href="https://www.linux.org" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/linux-colored.svg" width="36" height="36"/></a>
+<a href="https://www.microsoft.com/windows/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg" width="36" height="36"/></a>
+
+## DevOps ToolChain
+<a href="https://www.docker.com/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/docker-colored.svg" width="36" height="36"/></a>
+<a href="https://kubernetes.io/" target="_blank"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/kubernetes-colored.svg" width="36" height="36"/></a>
+<a href="https://www.terraform.io/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/terraform/terraform-original.svg" width="36" height="36"/></a>
+</details>
+
+<a href="https://www.gitanimals.org/en_US?utm_medium=image&utm_source=yjshin-cloud&utm_content=farm">
+  <img src="https://render.gitanimals.org/farms/yjshin-cloud" width="600" height="300" />
+</a>
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/yjshin-cloud/yjshin-cloud/output/pacman-contribution-graph-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/yjshin-cloud/yjshin-cloud/output/pacman-contribution-graph.svg">
+  <img alt="Pac-Man contribution graph" src="https://raw.githubusercontent.com/yjshin-cloud/yjshin-cloud/output/pacman-contribution-graph.svg">
+</picture>
+
+---
+
+## 📕 Latest Blog Posts
+`;
+
+    // 🔗 Tistory 최신글 10개 추가
+    text += `<ul>\n`;
+    feed.items.slice(0, 10).forEach((item) => {
+      text += `  <li><a href='${item.link}' target='_blank'>${item.title}</a></li>\n`;
+    });
+    text += `</ul>\n`;
+
+    // 파일 쓰기
+    writeFileSync("README.md", text, "utf8");
+    console.log("✅ README.md 업데이트 완료!");
+  } catch (err) {
+    console.error("❌ RSS 파싱 중 오류:", err);
+    process.exit(1);
   }
-
-  text += `</ul>`;
-
-  // README.md 파일 생성
-  writeFileSync("README.md", text, "utf8", (e) => {
-    console.log(e);
-  });
-  console.log("업데이트 완료");
 })();
